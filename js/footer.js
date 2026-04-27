c50f4696518af0b080b3c157d47cd65e623d86df
MEBZMasondo <mondlime8@gmail.com>
2026-04-27 21:13:46 +0200

    fix header and footer between localhost and github pages

--- js/footer.js
+++ js/footer.js
@@ -12,8 +12,28 @@
 //     .catch(error => console.error("Footer loading failed:", error));
 // });
 
+// document.addEventListener("DOMContentLoaded", () => {
+//   fetch("/profileMEBZ/components/footer.html")
+//     .then(response => response.text())
+//     .then(data => {
+//       document.getElementById("footer-placeholder").innerHTML = data;
+
+//       // Set current year after footer loads
+//       document.getElementById("year").textContent =
+//         new Date().getFullYear();
+//     })
+//     .catch(error => console.error("Footer loading failed:", error));
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("/profileMEBZ/components/footer.html")
+
+  const isGitHubPages = window.location.hostname.includes("github.io");
+
+  const BASE_PATH = isGitHubPages
+    ? "/profileMEBZ/"
+    : "/";
+
+  fetch(BASE_PATH + "components/footer.html")
     .then(response => response.text())
     .then(data => {
       document.getElementById("footer-placeholder").innerHTML = data;
@@ -23,4 +43,5 @@ document.addEventListener("DOMContentLoaded", () => {
         new Date().getFullYear();
     })
     .catch(error => console.error("Footer loading failed:", error));
+
 });
--- js/header.js
+++ js/header.js
@@ -7,11 +7,28 @@
 //     .catch(error => console.error("Header loading failed:", error));
 // });
 
+// document.addEventListener("DOMContentLoaded", () => {
+//   fetch("/profileMEBZ/components/header.html")
+//     .then(response => response.text())
+//     .then(data => {
+//       document.getElementById("header-placeholder").innerHTML = data;
+//     })
+//     .catch(error => console.error("Header loading failed:", error));
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("/profileMEBZ/components/header.html")
+
+  const isGitHubPages = window.location.hostname.includes("github.io");
+
+  const BASE_PATH = isGitHubPages
+    ? "/profileMEBZ/"
+    : "/";
+
+  fetch(BASE_PATH + "components/header.html")
     .then(response => response.text())
     .then(data => {
       document.getElementById("header-placeholder").innerHTML = data;
     })
     .catch(error => console.error("Header loading failed:", error));
+
 });
